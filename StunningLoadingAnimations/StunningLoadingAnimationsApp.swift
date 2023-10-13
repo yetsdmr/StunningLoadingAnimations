212f45c21886f82981c532542e005808b3cc2297
Yunus Emre Taşdemir <yetsdmr@gmail.com>
2023-10-13 11:27:17 +0300

    Initial Commit

---
+++ StunningLoadingAnimations/StunningLoadingAnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  StunningLoadingAnimationsApp.swift
+//  StunningLoadingAnimations
+//
+//  Created by Yunus Emre Taşdemir on 13.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct StunningLoadingAnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
